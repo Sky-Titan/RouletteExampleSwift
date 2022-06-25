3af7b2727f2cd54202750448c13227017a645511
SkyTitan <er65119@naver.com>
2022-06-25 13:10:55 +0900

    Add delegate

--- RouletteExample/SpinWheelView.swift
+++ RouletteExample/SpinWheelView.swift
@@ -7,18 +7,36 @@
 
 import UIKit
 
+protocol SpinWheelViewDelegate: AnyObject {
+    func spinWheelWillStart(_ spinWheelView: SpinWheelView)
+    func spinWheelDidEnd(_ spinWheelView: SpinWheelView, at index: Int)
+}
+
 class SpinWheelView: UIView {
     override class var layerClass: AnyClass {
         return SpinWheelLayer.self
     }
+    weak var delegate: SpinWheelViewDelegate?
     
     var spinWheelLayer: SpinWheelLayer? {
         return layer as? SpinWheelLayer
+    }
+    
+    func beginTo(_ index: Int) {
+        delegate?.spinWheelWillStart(self)
+        
     }
     
 }
 
 
 class SpinWheelLayer: CALayer {
+    
+    override func draw(in ctx: CGContext) {
+        
+    }
+}
+
+class SliceLayer: CALayer {
     
 }
